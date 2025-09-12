92d42f4c52307ff44e60b2fe48135a367db29082
cookiebouquets <silva@bemaniso.ws>
2025-09-12 10:18:34 -0400

    Update calc2.py

--- sec 1/calc2.py
+++ sec 1/calc2.py
@@ -3,7 +3,9 @@ How would we make a calculator where we can choose the operation at the start?
 
     1) Ask which operation they want to do
         - Think about what has to be done for this to work
+        - If elif after taking in option value through input
     2) Just do the calculator thing after that 
+        - If option == "addition": ....
 """
 
 print("welcome to the calculator!")
@@ -11,4 +13,5 @@ print("1. Addition")
 print("2. Subtraction")
 print("3. Multiplication")
 print("4. Division")
-#Fill in the rest of this code 
+#Fill in the rest of this code
+#You probably need to take an input and set that equal to some variable called option 
