a6ca443e8a584a670ed04ad0082737a11594b36b
cookiebouquets <silva@bemaniso.ws>
2025-10-23 14:11:40 -0400

    Create palidromehint.py

---
+++ sec 2/palidromehint.py
@@ -0,0 +1,30 @@
+""" 
+strings are lists (iterable) 
+
+we can do for-loops over strings
+
+for-loops pull out one character at a time 
+
+
+"""
+str1 = "hello"
+str2 = "world"
+print(str1 + str2)
+print(str2 + str1)
+
+
+reverse = "" #<= this is the empty string 
+letter1 = "a"
+letter2 = "b"
+
+
+
+print( letter1 + letter2 + reverse) #ab
+print(letter2 + letter1 + reverse) #ba 
+
+""" 
+after we save the word backwards into reverse using for-loop, the empty string, and concatenation, 
+
+all we need to do is what? we can check equality with an if-statement 
+
+"""
