fc70f69d55fcb15443cb5311a610a512a9eb3244
cookiebouquets <silva@bemaniso.ws>
2025-10-24 10:14:46 -0400

    commit

--- sec 2/palidromehint.py
+++ sec 2/palidromehint.py
@@ -7,6 +7,37 @@ for-loops pull out one character at a time
 
 
 """
+
+word = "hello"
+
+for char in word: #creates a temporary variable corresponding to each individual character 
+                  #inside of word  
+   print(char)
+
+
+#that means square bracket operators are fair game
+
+print(word[0]) #print first character
+
+
+""" 
+first character: 0
+last character: 4
+
+length of a list is denoted as len(list) 
+
+len(list) is which index? 
+
+len(word) = 5 but there's no fifth index i can do 
+
+len(word) - 1 <= this will actually get the last index 
+
+
+word[0] <= first character
+word[len(word) - 1] <= last character
+""" 
+
+
 str1 = "hello"
 str2 = "world"
 print(str1 + str2)
@@ -19,7 +50,7 @@ letter2 = "b"
 
 
 
-print( letter1 + letter2 + reverse) #ab
+print(letter1 + letter2 + reverse) #ab
 print(letter2 + letter1 + reverse) #ba 
 
 """ 
@@ -55,4 +86,44 @@ while loop and a counting variable, the counting variable corresponds to the ind
 in the word
 
 and exit the loop as soon as the word is no longer a palindrome 
+
+if we had a while loop with count variable as i = 0 initially, 
+
+what should our exit condition be for our while loop?
+
+if we're going to use i as our exit condition, what should the last number be for i? I only need
+to iterate through the first half
+
+while i < len(size) - 1 rather than doing this, i can
+
+while i < (len(size) - 1)//2  <= this gets HALF of the indices inside our word 
+
+i = 0
+
+word[len(word) - 1] <= this is the last character
+word[len(word) - 2]<= this is the second to last one
+ 
+ 
+assume i = [0,1,2,3] 
+ 
+word[len(word) - 1 - i] <= this is going to count downwards
+
+check = True
+
+
+racecar
+r     r
+ a   a
+  c c
+   e
+
+
+while i < (len(size) - 1)//2 and check == True:
+   if word[i] == word[len(word) - 1 - i]:
+      i = i + 1
+   else: 
+      check = False
+
+ 
 """
+
---
+++ sec 2/palindrometest.py
@@ -0,0 +1,7 @@
+word = "taco cat".split()
+word = "".join(word)
+word = list(word)
+reverse = (list(reversed(word)))
+print(word == reverse)
+
+
