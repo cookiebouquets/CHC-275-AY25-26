804b51e83e778e11a9aebaa456793b69905f05d7
cookiebouquets <silva@bemaniso.ws>
2025-10-24 17:30:23 -0400

    Update palindrometest.py

--- sec 2/palindrometest.py
+++ sec 2/palindrometest.py
@@ -1,7 +1,8 @@
-word = "taco cat".split()
+word = input("enter a word").lower().strip().split()
 word = "".join(word)
 word = list(word)
 reverse = (list(reversed(word)))
 print(word == reverse)
 
 
+print("A"* 8)
