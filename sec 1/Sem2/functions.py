e0c4eda459876f1f739cbe9a6388c9d9cce6a3e2
cookiebouquets <silva@bemaniso.ws>
2026-01-22 09:39:21 -0500

    Update functions.py

--- sec 1/Sem2/functions.py
+++ sec 1/Sem2/functions.py
@@ -274,3 +274,26 @@ balance = 500
 print(balance)
 balance = updateBalance(balance,200) #just like using input, we need to assign this value back into a variable
 print(balance)
+
+
+
+"""
+Programming drill 2:
+    - call the file drill2.py
+    - write a function called makeList() that
+        - takes in numbers via input() and typecasts them to integers, then appends them to a list called
+        "nums" UNTIL the user types in "stop"
+        - returns that list
+        
+    - try making two lists using makeList() and print those lists out
+"""
+
+
+def makeList():
+    nums = []
+    pass
+
+list1 = makeList()
+print(list1)
+list2 = makeList()
+print(list2)
